611034423d092eda70f31e4749fc2f2f09e8b976
Cinthya <cinthya.lorena.gonzalez@hotmail.com>
2022-11-26 08:55:55 -0600

    post api/users funciona, guarda y valida repetidos

--- BACKEND/server.mjs
+++ BACKEND/server.mjs
@@ -3,8 +3,8 @@ import express from "express";
 import chalk from "chalk";
 import cors from "cors";
 
-const app=express();
-const port=3000;
+const app = express();
+const port = 3000;
 
 app.use(cors({
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
@@ -44,8 +44,26 @@ let userSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    Date:{
-        type: Date,
+    Nacimiento:{
+        type: String,
+        required: true
+    },
+    Actividad: {
+        type: String,
+        enum: ["BAJA", "MEDIA", "ALTA"],
+        required: true
+    },
+    Region: {
+        type: String,
+        enum: ["FRÍA", "TEMPLADA", "CALUROSA"],
+        required: true
+    },
+    Peso: {
+        type: Number,
+        required: true
+    },
+    Estatura: {
+        type: Number,
         required: true
     },
     Sexo:{
@@ -53,8 +71,11 @@ let userSchema = mongoose.Schema({
         enum:['M','H'],
         required: true
     },
-    UID: {
-        type: Number,
+    StatsData: {
+        type: Boolean
+    },
+    UrlPicture: {
+        type: String,
         required: true
     }
 })
@@ -81,25 +102,154 @@ let liquidSchema = mongoose.Schema({
 })
 
 // ------DECLARACION DE SCHEMAS-----------------------------------------------------------------------------------
-let User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema); //para registro y edicion
 let Liquid = mongoose.model('Liquid',liquidSchema);
 
 // ---------------------------------------------------------------------------------------------------------------
 
 
-
-// PUBLICAR USUARIO
+//  ----- REGISTRAR USUARIO ---------
+/* POST /api/users
+  Se utiliza en el registro de usuarios. Debe verificar que no falte de rellenar ningún campo
+  y que tampoco exista el correo y/o el nombre en la base de datos.
+  STATUS CODES:
+   400 si falta información
+   401 si el correo o el nombre ya existen
+   201 si se registró correctamente
+*/
 app.post('/api/users',(req,res)=> {
-    let newUser =   {Nombre: req.body.Nombre, 
-        Apellido: req.body.Apellido,
-        Correo: req.body.Correo,
-        Contrasenia:req.body.Contrasenia,
-        Date:req.body.Date,
-        Sexo:req.body.Sexo,
-        UID:req.body.UID};
-    let user = User(newUser);
-    user.save().then((doc)=> console.log(chalk.green("Usuario Creado: ")+ doc));
+    let errorStr = [];
+    let missing = false;
+
+
+    if(req.body.Nombre == undefined || req.body.Nombre == "") {
+        errorStr.push("Nombre");
+        missing = true;
+    }
+    if(req.body.Apellido == undefined || req.body.Apellido == "") {
+        errorStr.push("Apellido");
+        missing = true;
+    }
+    if(req.body.Correo == undefined || req.body.Correo == "") {
+        errorStr.push("Correo");
+        missing = true;
+    }
+    if(req.body.Contrasenia == undefined || req.body.Contrasenia == "") {
+        errorStr.push("Contrasenia");
+        missing = true;
+    }
+    if(req.body.Nacimiento == undefined || req.body.Nacimiento == "") {
+        errorStr.push("Nacimiento");
+        missing = true;
+    }
+    if(req.body.Actividad == undefined || req.body.Actividad == "") {
+        errorStr.push("Actividad");
+        missing = true;
+    }
+    if(req.body.Region == undefined || req.body.Region == "") {
+        errorStr.push("Region");
+        missing = true;
+    }
+    if(req.body.Peso == undefined || req.body.Peso == "") {
+        errorStr.push("Peso");
+        missing = true;
+    }
+    if(req.body.Estatura == undefined || req.body.Estatura == "") {
+        errorStr.push("Estatura");
+        missing = true;
+    }
+    if(req.body.Sexo == undefined || !(req.body.Sexo == "M" || req.body.Sexo == "H")) {
+        errorStr.push("Sexo");
+        missing = true;
+    }
+    if(req.body.StatsData == undefined || !(req.body.StatsData == true || req.body.StatsData == false)) {  
+        errorStr.push("StatsData");
+        missing = true;
+    }
+    // Si falta algún campo:
+    if(missing) {
+        res.status(400).send(`Falta la siguiente información: ${errorStr.join(", ")}.`);
+        return;
+    }
+    // Si no falta nada:
+    else {
+        // Verificar que no exista el correo:
+        User.find({Correo : req.body.Correo},(err,docs)=>{
+            if(err) {
+                res.status(500).send("Error al verificar el correo.");
+                return;
+            }
+            // Si el correo ya existe:
+            if(docs.length > 0) {
+                res.status(401).send("El correo ya existe.");
+                return;
+            }
+            // Si el correo no existe:
+            else {
+                // Verificar que no exista el nombre:
+                User.find({Nombre : req.body.Nombre},(err,docs)=>{
+                    if(err) {
+                        res.status(500).send("Error al verificar el nombre.");
+                        return;
+                    }
+                    // Si el nombre ya existe:
+                    if(docs.length > 0) {
+                        res.status(401).send("El nombre ya existe.");
+                        return;
+                    }
+                    // Si el nombre no existe:
+                    else {
+                        // Hacer url random en caso de que se necesite
+                        let newUserUrl = "";
+                        if(req.body.UrlPicture == undefined || req.body.UrlPicture == "") {
+                            let num = Math.floor(Math.random() * 100);
+                            req.body.Sexo.toUpperCase() == "H" ?
+                            newUserUrl = `https://randomuser.me/api/portraits/men/${num}.jpg` :
+                            newUserUrl = `https://randomuser.me/api/portraits/women/${num}.jpg`;
+                        }
+                        else {
+                            // Dejar la que puso el usuario
+                            newUserUrl = req.body.UrlPicture;
+                        }
+
+                        // Crear el usuario:
+                        let newUser = new User({
+                            Nombre: req.body.Nombre,
+                            Apellido: req.body.Apellido,
+                            Correo: req.body.Correo,
+                            Contrasenia: req.body.Contrasenia,
+                            Nacimiento: req.body.Nacimiento,
+                            Actividad: req.body.Actividad,
+                            Region: req.body.Region,
+                            Peso: req.body.Peso,
+                            Estatura: req.body.Estatura,
+                            Sexo: req.body.Sexo,
+                            StatsData: req.body.StatsData,
+                            UrlPicture: newUserUrl
+                        });
+                        // Guardar el usuario:
+                        newUser.save((err, doc)=>{
+                            if(err) {
+                                res.status(500).send("Error al guardar el usuario.");
+                                return;
+                            }
+                            // Si se guardó correctamente:
+                            else {
+                                res.status(201).send("Usuario registrado correctamente.");
+                                console.log(doc);
+                                return;
+                            }
+                        });
+                    }
+                });
+            
+            }
+
+        });
+    }
 });
+
+
 
 
 // PUBLICAR BEBIDA
